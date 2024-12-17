a239ae772ce192848b78876ba6409e362bdfd6ec
kaarl <kaarl@KAKE_PC-1>
2024-12-17 17:47:37 +0200

    Add project files.

---
+++ Kivi_sakset_paperi__KW/Kivi_sakset_paperi__KW/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int Pisteet = 0;
+            int ai_Pisteet = 0; //making an iteger to store the match's points
+
+            Console.WriteLine("Kivi, sakset, paperi!");
+
+            while (Pisteet != 3 && ai_Pisteet != 3)//creating a loop to prevent the program from stopping until one player scores 3 points
+            {
+                Console.WriteLine("Sinun pisteet: " + Pisteet + " | Koneen pisteet: " + ai_Pisteet);
+                Console.WriteLine("valitse: kivi 'k', sakset 's' tai paperi 'p'"); // give each choice
+                string valinta = Console.ReadLine(); // store the player's choice in string
+
+                int kone_valinta = random.Next(0, 2); // Computer chooses 0 - 2 randomly 
+
+                if (kone_valinta == 0)
+                {
+                    Console.WriteLine("Kone valitsi kiven.");
+
+                    switch (valinta)
+                    {
+                        case "k":
+                            Console.WriteLine("Valitsitte molemmat kiven.");
+                            break;
+                        case "p":
+                            Console.WriteLine("+1 piste");
+                            Pisteet++;
+                            break;
+                        default:
+                            Console.WriteLine("+1 piste koneelle");
+                            ai_Pisteet++;
+                            break;
+
+                    }
+                }
+                else if (kone_valinta == 1)
+                {
+                    Console.WriteLine("Kone valitsi sakset.");
+                    switch (valinta)
+                    {
+                        case "k":
+                            Console.WriteLine("1+ piste");
+                            Pisteet++;
+                            break;
+                        case "p":
+                            Console.WriteLine("+1 piste koneelle");
+                            ai_Pisteet++;
+                            break;
+                        default:
+                            Console.WriteLine("Valitsitte molemmat sakset.");
+                            break;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Kone valitsi paperin.");
+                    switch (valinta)
+                    {
+                        case "k":
+                            Console.WriteLine("+1 piste koneelle");
+                            Pisteet++;
+                            break;
+                        case "p":
+                            Console.WriteLine("Valitsitte molemmat paperin.");
+                            break;
+                        default:
+                            Console.WriteLine("+1 piste");
+                            Pisteet++;
+                            break;
+
+                    }
+                }
+
+            }
+            if (Pisteet == 3)
+            {
+                Console.WriteLine("Voitit, ensikertaan!");
+            }
+            else
+            {
+                Console.WriteLine("Hävisit, ensikertaan!");
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laskin_KW/Laskin_KW/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laskin_KW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;   //using integer to store the number
+            int num2;
+
+            string vastaus;
+            int result;
+
+            Console.WriteLine("Tervetuloa laskimeen."); //writing welcome and asking for the first number
+            Console.WriteLine("Anna numero");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("anna toinen numero");
+
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Minkä tyyppinen lasku? y jos haluat yhteenlaskun, v jos haluat vähennyslaskun, k jos haluat kertolaskun ja j jos haluat jakolaskun "); //asking for wich type of calculation we are running
+
+            vastaus = Console.ReadLine(); //response to the previous question in string format
+
+            if (vastaus == "y") //the calculations
+            {
+                result = num1 + num2;
+                Console.WriteLine("Vastaus = " + result);
+            }
+            else if (vastaus == "v")
+            {
+                result = num1 - num2;
+                Console.WriteLine("Vastaus = " + result);
+            }
+            else if (vastaus == "k")
+            {
+                result = num1 * num2;
+                Console.WriteLine("Vastaus = " + result);
+            }
+            else if (vastaus == "j")
+            {
+                result = num1 / num2;
+                Console.WriteLine("Vastaus = " + result);
+            }
+            else
+            {
+                Console.WriteLine("Väärä vastaus");
+            }
+
+            // Console.WriteLine("Vastaus = " + result); i could not figure out why this doesnt work here
+            Console.WriteLine("Kiitos käynnistä");
+
+
+            Console.ReadKey();
+        }
+    }
+}
